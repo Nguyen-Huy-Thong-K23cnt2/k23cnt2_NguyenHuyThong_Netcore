42974b5ae7f843265fe3e1f1a93630462b387a63
unknown <bhuythong04@gmail.com>
2025-04-27 16:26:29 +0700

    btap1

---
+++ MyAppNHT/Controllers/LienHe.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAppNHT.Controllers
+{
+    public class LienHe : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
